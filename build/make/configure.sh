90300422358755ba0bb4ea517148389875289d75
William King <william.king@quentustech.com>
2015-05-15 16:54:23 -0700

    Missed one patch

--- build/make/configure.sh
+++ build/make/configure.sh
@@ -825,6 +825,7 @@ process_common_toolchain() {
 
       case ${tgt_cc} in
         gcc)
+          CROSS=${CROSS:-arm-none-linux-gnueabi-}
           link_with_cc=gcc
           setup_gnu_toolchain
           arch_int=${tgt_isa##armv}
