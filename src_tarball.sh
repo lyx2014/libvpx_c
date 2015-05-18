33ca51af2808a459286b2c40e4db3c78b949f785
Chris Rienzo <chris.rienzo@grasshopper.com>
2015-05-18 09:55:39 -0400

    add RPM packaging

--- src_tarball.sh
+++ src_tarball.sh
@@ -5,10 +5,10 @@ TMPDIR=libvpx.$$
 mkdir -p ../${TMPDIR}
 
 cd ..
-cp -a libvpx ${TMPDIR}/libvpx-1.3.0
+cp -a libvpx ${TMPDIR}/libvpx-1.4.0
 cd ${TMPDIR}
-rm -rf libvpx-1.3.0/.git*
-tar zcvf libvpx-1.3.0.tar.gz libvpx-1.3.0
-mv libvpx-1.3.0.tar.gz ${ORIGDIR}/.
+rm -rf libvpx-1.4.0/.git*
+tar zcvf libvpx-1.4.0.tar.gz libvpx-1.4.0
+mv libvpx-1.4.0.tar.gz ${ORIGDIR}/.
 cd ${ORIGDIR}
 rm -rf ../${TMPDIR}
